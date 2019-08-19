0cd8b06b11d2835159f868a40cfb75ae49b4101f
May Ng <mngmay@gmail.com>
2019-08-19 14:54:22 -0700

    wrote test to confirm buttons exist for controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -11,8 +11,20 @@ describe("<Controls />", () => {
     expect(tree.toJSON()).toMatchSnapshot();
   });
 
-  it("displays buttons to toggle closed and locked", () => {});
-  it("displays change in text when buttons are clicked based on state", () => {});
+  it("displays buttons to toggle closed and locked", () => {
+    const controls = render(<Controls />);
+    const lockedBtn = controls.getByTestId("lockedBtn");
+    const openBtn = controls.getByTestId("openBtn");
+
+    expect(lockedBtn).toBeTruthy();
+    expect(openBtn).toBeTruthy();
+  });
+  it("displays change in text when buttons are clicked based on state", () => {
+    const state = {
+      locked: false,
+      closed: false
+    };
+  });
   it("disables closed toggle button if gate is locked", () => {});
   it("disables locked toggle button if gate is open", () => {});
 });
